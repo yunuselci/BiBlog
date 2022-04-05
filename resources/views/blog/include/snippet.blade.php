c44055b34f34bb1fed2e2df2c7038becaa8f9128
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2022-04-05 13:51:36 +0300

    Changes on icon

--- resources/views/blog/include/snippet.blade.php
+++ resources/views/blog/include/snippet.blade.php
@@ -55,7 +55,7 @@
                     <div class=" max-w-5xl mx-auto flex items-center justify-center h-screen">
                         <div class="px-40 py-20 bg-white rounded-md shadow-xl">
                             <div class="flex flex-col items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="max-h-60 h-60 w-60 text-blue-600" fill="none"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" class="text-blue-600" fill="none"
                                      viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
