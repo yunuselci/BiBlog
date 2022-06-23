5b9de94cbcb90b255ffa815ab233be097cd20edc
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2022-06-24 01:38:58 +0300

    Cs Fix

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -9,51 +9,62 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
-use Laravel\Nova\Observable;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         parent::boot();
 
-        Nova::serving(function () {
+        Nova::serving(function (): void {
             Post::observe(PostObserver::class);
         });
     }
 
     /**
-     * Register the Nova routes.
+     * Get the tools that should be listed in the Nova sidebar.
      *
-     * @return void
+     * @return array
      */
-    protected function routes()
+    public function tools()
+    {
+        return [];
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Register the Nova routes.
+     */
+    protected function routes(): void
     {
         Nova::routes()
-                ->withAuthenticationRoutes()
-                ->withPasswordResetRoutes()
-                ->register();
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register()
+        ;
     }
 
     /**
      * Register the Nova gate.
      *
      * This gate determines who can access Nova in non-local environments.
-     *
-     * @return void
      */
-    protected function gate()
+    protected function gate(): void
     {
-        //TODO: make more meaningful gate for nova users
+        // TODO: make more meaningful gate for nova users
         Gate::define('viewNova', function (?User $user) {
-            return in_array($user->email, [
+            return \in_array($user->email, [
                 'guvenatbakan@gmail.com',
-            ]);
+            ], true);
         });
     }
 
@@ -77,24 +88,5 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function dashboards()
     {
         return [];
-    }
-
-    /**
-     * Get the tools that should be listed in the Nova sidebar.
-     *
-     * @return array
-     */
-    public function tools()
-    {
-        return [];
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
     }
 }
