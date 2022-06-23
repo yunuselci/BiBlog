55ea41720e887fa836a18f2b412ab06dc32a0cbc
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2022-06-24 01:21:34 +0300

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,71 +1,25 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of PHP CS Fixer.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
-$header = <<<'EOF'
-    This file is part of PHP CS Fixer.
-
-    (c) Fabien Potencier <fabien@symfony.com>
-        Dariusz Rumiński <dariusz.ruminski@gmail.com>
-
-    This source file is subject to the MIT license that is bundled
-    with this source code in the file LICENSE.
-    EOF;
-
 $finder = PhpCsFixer\Finder::create()
-    ->ignoreDotFiles(false)
-    ->ignoreVCSIgnored(true)
-    ->exclude('tests/Fixtures')
-    ->in(__DIR__)
-    ->append([
-        __DIR__.'/dev-tools/doc.php',
-        // __DIR__.'/php-cs-fixer', disabled, as we want to be able to run bootstrap file even on lower PHP version, to show nice message
-    ])
-;
+    ->in(__DIR__ . '/app')
+    ->in(__DIR__ . '/tests')
+    ->in(__DIR__ . '/database')
+    ->in(__DIR__ . '/config')
+    ->in(__DIR__ . '/routes');
 
 $config = new PhpCsFixer\Config();
 $config
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PHP74Migration' => true,
-        '@PHP74Migration:risky' => true,
+        '@Symfony' => true,
+        '@PSR2' => true,
+        'array_syntax' => ['syntax' => 'short'],
+
+        '@PHP71Migration:risky' => true,
         '@PHPUnit75Migration:risky' => true,
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
-        'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']], // one should use PHPUnit built-in method instead
-        'header_comment' => ['header' => $header],
-        'heredoc_indentation' => false, // TODO switch on when # of PR's is lower
-        'modernize_strpos' => true, // needs PHP 8+ or polyfill
-        'use_arrow_functions' => false, // TODO switch on when # of PR's is lower
+        'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']],
     ])
-    ->setFinder($finder)
-;
-
-// special handling of fabbot.io service if it's using too old PHP CS Fixer version
-if (false !== getenv('FABBOT_IO')) {
-    try {
-        PhpCsFixer\FixerFactory::create()
-            ->registerBuiltInFixers()
-            ->registerCustomFixers($config->getCustomFixers())
-            ->useRuleSet(new PhpCsFixer\RuleSet($config->getRules()))
-        ;
-    } catch (PhpCsFixer\ConfigurationException\InvalidConfigurationException $e) {
-        $config->setRules([]);
-    } catch (UnexpectedValueException $e) {
-        $config->setRules([]);
-    } catch (InvalidArgumentException $e) {
-        $config->setRules([]);
-    }
-}
-
+    ->setFinder($finder);
 return $config;
